3d73ed792d028e47d353412fb83c0298a379d38c
khleeloo <frieske@gmail.com>
2022-09-30 11:20:04 +0800

    run script

---
+++ run_seathru.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python  seathru.py \
+    --image '/home/khleeloo/code/uwh_project/sea-thru/T_S04859.jpg'  \
+    --depth-map '/home/khleeloo/code/uwh_project/sea-thru/depthT_S04859.jpg' --output "output.png"
+
+
