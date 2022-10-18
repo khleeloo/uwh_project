ff20e4db2c9b02005f84aa58194cbf9c72710fd8
khleeloo <frieske@gmail.com>
2022-10-18 16:21:41 +0800

    seathru_mono

---
+++ run_seathru_mono.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python  seathru-mono-e2e.py \
+    --image '/home/rita/uwh_project/justin/datasets/Underwater_PairedData256_filtered/train/euvp_dark_1.png'  \
+     --output "output_euvp_dark_1.png" \
+    --output-graphs --monodepth
+
+
