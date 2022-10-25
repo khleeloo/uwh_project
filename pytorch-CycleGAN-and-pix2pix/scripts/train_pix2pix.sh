69d774876c79f10fe18debf1bfea3aeeb518ba97
khleeloo <frieske@gmail.com>
2022-10-25 18:57:20 +0800

    training script

--- pytorch-CycleGAN-and-pix2pix/scripts/train_pix2pix.sh
+++ pytorch-CycleGAN-and-pix2pix/scripts/train_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0
+python train.py --dataroot /home/rita/uwh_project/justin/datasets/Underwater_PairedData256_filtered --name facades_pix2pix --model pix2pix --netG unet_256 --direction AtoB --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0 --gpu_ids 0,1
