17198e02d461457c752d81399164ec371b933623
khleeloo <frieske@gmail.com>
2022-11-23 11:48:23 +0800

    metrics folder

---
+++ Metrics/evaluate.py
@@ -0,0 +1,287 @@
+'''
+Metrics for unferwater image quality evaluation.
+
+
+Usage:
+python evaluate.py RESULT_PATH REFERENCE_PATH
+'''
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+import math
+import sys
+from skimage import io, color, filters
+import os
+import math
+import argparse
+import gym
+from itertools import count
+from collections import namedtuple
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+import pandas as pd
+
+def rmetrics(a,b):
+    
+    #pnsr
+    mse = np.mean((a-b)**2)
+    psnr = 10*math.log10(1/mse)
+
+    #ssim
+    ssim = structural_similarity(a,b,channel_axis=3)
+
+    return psnr, ssim
+
+def nmetrics(a):
+    rgb = a
+    lab = color.rgb2lab(a)
+    gray = color.rgb2gray(a)
+    # UCIQE
+    c1 = 0.4680
+    c2 = 0.2745
+    c3 = 0.2576
+    l = lab[:,:,0]
+
+    #1st term
+    chroma = (lab[:,:,1]**2 + lab[:,:,2]**2)**0.5
+    uc = np.mean(chroma)
+    sc = (np.mean((chroma - uc)**2))**0.5
+
+    #2nd term
+    top = int(np.round(0.01*l.shape[0]*l.shape[1]))
+    sl = np.sort(l,axis=None)
+    isl = sl[::-1]
+    conl = np.mean(isl[:top])-np.mean(sl[:top])
+
+    #3rd term
+    satur = []
+    chroma1 = chroma.flatten()
+    l1 = l.flatten()
+    for i in range(len(l1)):
+        if chroma1[i] == 0: satur.append(0)
+        elif l1[i] == 0: satur.append(0)
+        else: satur.append(chroma1[i] / l1[i])
+
+    us = np.mean(satur)
+
+    uciqe = c1 * sc + c2 * conl + c3 * us
+
+    # UIQM
+    p1 = 0.0282
+    p2 = 0.2953
+    p3 = 3.5753
+
+    #1st term UICM
+    rg = rgb[:,:,0] - rgb[:,:,1]
+    yb = (rgb[:,:,0] + rgb[:,:,1]) / 2 - rgb[:,:,2]
+    rgl = np.sort(rg,axis=None)
+    ybl = np.sort(yb,axis=None)
+    al1 = 0.1
+    al2 = 0.1
+    T1 = int(al1 * len(rgl))
+    T2 = int(al2 * len(rgl))
+    rgl_tr = rgl[T1:-T2]
+    ybl_tr = ybl[T1:-T2]
+
+    urg = np.mean(rgl_tr)
+    s2rg = np.mean((rgl_tr - urg) ** 2)
+    uyb = np.mean(ybl_tr)
+    s2yb = np.mean((ybl_tr- uyb) ** 2)
+
+    uicm =-0.0268 * np.sqrt(urg**2 + uyb**2) + 0.1586 * np.sqrt(s2rg + s2yb)
+
+    #2nd term UISM (k1k2=8x8)
+    Rsobel = rgb[:,:,0] * filters.sobel(rgb[:,:,0])
+    Gsobel = rgb[:,:,1] * filters.sobel(rgb[:,:,1])
+    Bsobel = rgb[:,:,2] * filters.sobel(rgb[:,:,2])
+
+    Rsobel=np.round(Rsobel).astype(np.uint8)
+    Gsobel=np.round(Gsobel).astype(np.uint8)
+    Bsobel=np.round(Bsobel).astype(np.uint8)
+
+    Reme = eme(Rsobel)
+    Geme = eme(Gsobel)
+    Beme = eme(Bsobel)
+
+    uism = 0.299 * Reme + 0.587 * Geme + 0.114 * Beme
+
+    #3rd term UIConM
+    uiconm = logamee(gray)
+
+    uiqm = p1 * uicm + p2 * uism + p3 * uiconm
+    return uiqm,uciqe
+
+def eme(ch,blocksize=8):
+
+    num_x = math.ceil(ch.shape[0] / blocksize)
+    num_y = math.ceil(ch.shape[1] / blocksize)
+    
+    eme = 0
+    w = 2. / (num_x * num_y)
+    for i in range(num_x):
+
+        xlb = i * blocksize
+        if i < num_x - 1:
+            xrb = (i+1) * blocksize
+        else:
+            xrb = ch.shape[0]
+
+        for j in range(num_y):
+
+            ylb = j * blocksize
+            if j < num_y - 1:
+                yrb = (j+1) * blocksize
+            else:
+                yrb = ch.shape[1]
+            
+            block = ch[xlb:xrb,ylb:yrb]
+
+            blockmin = float(np.min(block))
+            blockmax = float(np.max(block))
+
+            # # old version
+            # if blockmin == 0.0: eme += 0
+            # elif blockmax == 0.0: eme += 0
+            # else: eme += w * math.log(blockmax / blockmin)
+
+            # new version
+            if blockmin == 0: blockmin+=1
+            if blockmax == 0: blockmax+=1
+            eme += w * math.log(blockmax / blockmin)
+    return eme
+
+def plipsum(i,j,gamma=1026):
+    return i + j - i * j / gamma
+
+def plipsub(i,j,k=1026):
+    return k * (i - j) / (k - j)
+
+def plipmult(c,j,gamma=1026):
+    return gamma - gamma * (1 - j / gamma)**c
+
+def logamee(ch,blocksize=8):
+
+    num_x = math.ceil(ch.shape[0] / blocksize)
+    num_y = math.ceil(ch.shape[1] / blocksize)
+    
+    s = 0
+    w = 1. / (num_x * num_y)
+    for i in range(num_x):
+
+        xlb = i * blocksize
+        if i < num_x - 1:
+            xrb = (i+1) * blocksize
+        else:
+            xrb = ch.shape[0]
+
+        for j in range(num_y):
+
+            ylb = j * blocksize
+            if j < num_y - 1:
+                yrb = (j+1) * blocksize
+            else:
+                yrb = ch.shape[1]
+            
+            block = ch[xlb:xrb,ylb:yrb]
+            blockmin = float(np.min(block))
+            blockmax = float(np.max(block))
+            
+
+            top = plipsub(blockmax,blockmin)
+            bottom = plipsum(blockmax,blockmin)
+            
+
+            if bottom ==0:
+                print(bottom)
+                print('blockmax',blockmax, 'blockmin', blockmin)
+
+                s+=0
+            else:
+                m = top/bottom
+
+            if m ==0. :
+                s+=0
+            else:
+                s += (m) * np.log(m)
+
+    return plipmult(w,s)
+
+def main():
+
+    result_path = sys.argv[1]
+
+    result_files = os.listdir(result_path)
+
+    if len(sys.argv) >2 :
+        reference_path = sys.argv[2] 
+        reference_files = os.listdir(reference_path)
+    else:
+        reference_files=None
+
+
+
+    sumpsnr, sumssim, sumuiqm, sumuciqe = 0.,0.,0.,0.
+    
+
+
+    N=0
+    serieslist=[]
+    if reference_files is not None:
+        for imgfile,reffile in zip(result_files,reference_files):
+
+            corrected = io.imread(os.path.join(result_path,imgfile))
+
+    #       #reference image
+            imgname = imgfile.split('.png')[0]
+            # refdir = imgname+'.png'
+            reference = io.imread(os.path.join(reference_path,reffile))
+
+            psnr,ssim = rmetrics(corrected, reference)
+
+            uiqm,uciqe = nmetrics(corrected)
+            
+            series = pd.Series({"imgname":imgname,"psnr":psnr,"ssim":ssim,"uiqm":uiqm,"uciqe":uciqe  })
+            serieslist.append(series)
+            sumpsnr += psnr
+            sumssim += ssim
+            sumuiqm += uiqm
+            sumuciqe += uciqe
+            N +=1
+
+                
+    else:
+        for imgfile in result_files:
+            
+            corrected = io.imread(os.path.join(result_path,imgfile))
+          
+            imgname = imgfile.split('.png')[0]
+            uiqm,uciqe = nmetrics(corrected)
+            psnr, ssim =0,0
+            series = pd.Series({"imgname":imgname,"psnr":psnr,"ssim":ssim,"uiqm":uiqm,"uciqe":uciqe  })
+            serieslist.append(series)
+
+            sumuiqm += uiqm
+            sumuciqe += uciqe
+            N +=1
+
+        
+        myDf=pd.DataFrame(serieslist)
+       
+        if sumpsnr and sumssim>0:
+            mpsnr = sumpsnr/N
+            mssim = sumssim/N
+        else:
+            mpsnr, mssim=0, 0
+        muiqm = sumuiqm/N
+        muciqe = sumuciqe/N
+
+        myDf = myDf.append({"imgname":"Average:", "psnr":mpsnr,"ssim":mssim, "uiqm":muiqm,"uciqe":muciqe}, ignore_index=True)
+
+        myDf.to_csv(os.path.join(sys.path[0],'metrics.csv'))
+        
+        
+
+if __name__ == '__main__':
+    main()
---
+++ Metrics/preprocess.py
@@ -0,0 +1,179 @@
+"""
+num_channels.py
+Find number of channels in your image.
+Author: liuhh02 https://machinelearningtutorials.weebly.com/
+"""
+
+from PIL import Image
+import numpy as np
+
+# # name of your image file
+# filename = '/home/rita/uwh_project/justin/datasets/SQUID_original/image_set_01/LFT_3875resizedUndistort.tif'
+
+# # open image using PIL
+# img = Image.open(filename)
+
+# # convert to numpy array
+# img = np.array(img)
+
+# # find number of channels
+# if img.ndim == 2:
+#     channels = 1
+#     print("image has 1 channel")
+# else:
+#     channels = img.shape[-1]
+#     print("image has", channels, "channels")
+
+
+"""
+pix2pix_combine
+Combine 2 images from different domains for pix2pix. Make sure images in folderA and folderB have the same name.
+Folder Structure:
+folderA
+    |--> train
+    |--> valid (if any)
+    |--> test (if any)
+folderB
+    |--> train
+    |--> valid (if any)
+    |--> test (if any)
+dest_path
+    |--> train
+    |--> valid (if any)
+    |--> test (if any_
+Adapted from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+"""
+
+import os
+import numpy as np
+from PIL import Image
+import cv2
+import sys
+
+class Preprocess():
+    
+
+    def tile(self,*images, vertical=False):
+        width, height = images[0].width, images[0].height
+        tiled_size = (
+            (width, height * len(images))
+            if vertical
+            else (width * len(images), height)
+        )
+        tiled_img = Image.new(images[0].mode, tiled_size)
+        row, col = 0, 0
+        for image in images:
+            tiled_img.paste(image, (row, col))
+            if vertical:
+                col += height
+            else:
+                row += width
+
+        return tiled_img
+
+
+
+
+    def allign_stereo(self,folderA,folderB,dest_path):
+        splitsA = os.listdir(folderA)
+        splitsB = os.listdir(folderB)
+
+        for spa, spb in zip(splitsA,splitsB):
+            img_fold_A = os.path.join(folderA, spa)
+            img_fold_B = os.path.join(folderB, spb)
+            img_list_A = os.listdir(folderA)
+            img_list_B = os.listdir(folderB)
+            num_imgs = len(img_list_A)
+        
+            # img_fold_AB = os.path.join(dest_path, sp)
+            if not os.path.isdir(dest_path):
+                os.makedirs('output')
+            print('splitA = %s, number of images = %d' % (spa, num_imgs))
+
+            name_A = spa.split('.tif')[0] 
+            name_B=spb.split('.tif')[0] 
+            name_AB = name_A +'_'+ name_B + '.jpeg' 
+            path_AB = os.path.join(dest_path, name_AB)
+            im_A1 = Image.open(img_fold_A)
+            im_B1 = Image.open(img_fold_B)
+            im_AB=self.get_concat_h(im_A1,im_B1)
+            im_AB.save(path_AB,'JPEG',optimize=True )
+
+
+    def get_concat_h(self,im1, im2):
+        dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+        dst.paste(im1, (0, 0))
+        dst.paste(im2, (im1.width, 0))
+        return dst
+
+    def normalize(self, arr):
+        '''Function to scale an input array to [-1, 1]'''
+        arr_min = arr.min()
+        arr_max = arr.max()
+        # Check the original min and max values
+        print('Min: %.3f, Max: %.3f' % (arr_min, arr_max))
+        arr_range = arr_max - arr_min
+        scaled = np.array((arr-arr_min) / float(arr_range), dtype='f')
+        arr_new = -1 + (scaled * 2)
+        # Make sure min value is -1 and max value is 1
+        print('Min: %.3f, Max: %.3f' % (arr_new.min(), arr_new.max()))
+        return arr_new
+
+    def scale(self,img_path):
+        for filename in listdir(img_path):
+            # load image
+            image = Image.open(img_path+'/'+ filename)
+            # convert to numpy array
+            image = np.array(image)
+            # scale to [-1,1]
+            image = self.normalize(image)
+            image.save(img_path+'/'+ filename)
+
+    def change_format(self,img_path):
+        for filename in listdir(img_path):
+            # load image
+            image = Image.open(img_path+'/'+ filename)
+            new_file=filename.split('.')[0]
+
+            image.save(img_path+'/'+ new_file+'.png')
+
+
+
+
+        
+
+"""
+scale_images.py
+Function to scale any image to the pixel values of [-1, 1] for GAN input.
+Author: liuhh02 https://machinelearningtutorials.weebly.com/
+"""
+from PIL import Image
+import numpy as np
+from os import listdir
+
+#
+
+def main():
+    '''specify input paths for stereo and output paths'''
+    # folderA = 'FolderA'
+    folderA = sys.argv[0]
+    # folderA='Output_from_Seathru'
+    folderB = sys.argv[1]
+    dest_path = sys.argv[2]
+    pr=Preprocess()
+    if len(sys.argv)>2:
+        pr.allign_stereo(folderA,folderB,dest_path)
+    else:
+        pr.change_format(folderA)
+    
+    
+    # define paths for translation from domain A (images in folderA) -> domain B (images in folderB)
+    # loop through all files in the directory
+
+
+
+
+
+if __name__=='__main__':
+    main()
+
