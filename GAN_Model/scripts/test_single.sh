5a7d3b6f16537ab9b914f0d70d807370f7b6a66a
khleeloo <frieske@gmail.com>
2022-11-27 18:13:52 +0800

    cleanning

--- GAN_Model/scripts/test_single.sh
+++ GAN_Model/scripts/test_single.sh
@@ -1,4 +1,4 @@
 
 
 set -ex
-python test.py --dataroot /home/rita/uwh_project/Datasets/stitch --name facades_pix2pix  --netG unet_256   --model test --norm batch  --gpu_ids 0
+python test.py --dataroot /home/rita/uwh_project/Datasets/stitch --name facades_pix2pix  --netG unet_256  --model test --norm batch  --gpu_ids 0
--- GAN_Model/scripts/train_pix2pix.sh
+++ GAN_Model/scripts/train_pix2pix.sh
@@ -1,2 +1,8 @@
 set -ex
-python train.py --dataroot /home/rita/uwh_project/Datasets/Underwater_PairedData256_filtered --name color_pix2pix --model pix2pix --netG unet_256 --direction AtoB --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0 --gpu_ids 1
+python train.py --dataroot /home/rita/uwh_project/Datasets//home/rita/uwh_project/Datasets/UWH_Showcase_video_frames/1_images \
+--name facades_pix2pix --checkpoints_dir './checkpoints' \
+--model pix2pix --netG unet_256 \
+--direction AtoB --lambda_L1 100 \
+--dataset_mode aligned \
+--n_epochs 200 --norm batch --pool_size 0 \
+--gpu_ids 1 --batch_size 16 --continue_train 
